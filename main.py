e7bf6a31ec418cfe654d62679fb1595417b11418
Z3ZEL <estebanrodriguez17780@gmail.com>
2022-12-07 17:05:54 +0100

    working

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-import numpy as np; 
 import pandas as pd
-import prince
-from sklearn import datasets
-
-data = pd.read_csv('res/nombre-origine.csv',encoding = 'ISO-8859-1', index_col=0)
-#data = pd.DataFrame(data=data, columns=["Juridiction","Origine","Année","Qualificatifs de données","Nombre (nul)"])
-
-#show
-data.head()
+data = pd.read_csv("res/nombre-origine.csv",encoding = 'utf-8', index_col=0)
+print(data.head())
